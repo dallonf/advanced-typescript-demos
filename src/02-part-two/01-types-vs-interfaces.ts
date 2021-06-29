822a82fde51405a0d0aef657002f2af5f4924b5a
Dallon Feldner <dallon.feldner@volusion.com>
2021-06-29 14:36:30 -0500

    Types vs interfaces

---
+++ src/02-part-two/01-types-vs-interfaces.ts
@@ -0,0 +1,35 @@
+// Interfaces declare a new type
+interface MyInterface {
+  a: string;
+  b: string;
+  c: string;
+}
+const objectOfMyInterface: MyInterface = {
+  a: "one",
+  b: "two",
+  c: "three",
+};
+
+// Types are essentially aliases - you give a name to a type expression that already exists
+type MyType = {
+  a: string;
+  b: string;
+  c: string;
+};
+const objectOfMyType: MyType = {
+  a: "one",
+  b: "two",
+  c: "three",
+};
+
+// They're very similar!
+class MyExtendedType implements MyType {
+  a: string = "one";
+  b: string = "two";
+  c: string = "three";
+}
+
+// Conclusion: Interfaces are basically just types with fewer features...
+// but they can often play better with intellisense
+
+export {};
